240da4f0b876289237aa2eca880aec576f93dad6
yabuki829 <syodai829@icloud.com>
2022-10-17 12:55:49 +0900

    Initial Commit

---
+++ nexflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nexflix-clone
+//
+//  Created by 薮木翔大 on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
